3cf71c4b3f959a13bfb31b0d8db8e3fd12cc4560
HamInGyu <hfire1207@gmail.com>
2022-03-01 00:03:34 +0900

    feat: 1.6 : _app.js를 활용, 글로벌하게 스타일, 레이아웃 적용하기

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import NavBar from "../components/NavBar";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <NavBar />
+      <Component {...pageProps} />
+      <style jsx global>{`
+        a {
+          color: white;
+        }
+      `}</style>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
