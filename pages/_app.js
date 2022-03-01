2815f419570ebfea46708ab5c7ba9e9e6bb61c50
HamInGyu <hfire1207@gmail.com>
2022-03-01 13:24:45 +0900

    feat: 2.1 : Layout패턴 -> Layout컴포넌트 만들기
    _app.js에 최대한 적은 코드를 유지하기 위해

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,11 @@
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-      <style jsx global>{`
-        a {
-          color: white;
-        }
-      `}</style>
-    </div>
+    </Layout>
   );
 }
 
