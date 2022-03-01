9b09e739aec4b4fb0d52228d97469c9ae7e92e08
HamInGyu <hfire1207@gmail.com>
2022-03-01 15:05:03 +0900

    feat: 2.3 : data fething, loading중 화면까지

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "57644f2e20cf1ba7f37fe8ae2dbb46bf";
 export default function Home() {
+  const [movies, setMovies] = useState(); //초기 state를 아예 빈배열도 아닌 undefined로!
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      );
+      const data = await response.json();
+      setMovies(data.results);
+    })(); //익명함수를 만들고 바로 실행시키기
+  }, []);
   return (
     <>
       <Seo title="Home" />
-      <h1>Home</h1>
+      {/*movies가 undefined면 Loading... 띄우기*/}
+      {!movies && <h4>Loading...</h4>}
+      {/*movies가 있으면? .map 실행*/}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </>
   );
 }
